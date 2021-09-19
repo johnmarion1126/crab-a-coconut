459e3eec50af02cf3a81cf9fbcf3123731f3f26a
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-09-19 14:05:37 -0700

    Create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// Dependencies
+use ggez::event;
+use ggez::graphics;
+use ggez::{Context, GameResult};
+
+fn main() -> GameResult {
+    let context_builder = ggez::ContextBuilder::new("Crab-A-Coconut", "John");
+    let (ctx, event_loop) = context_builder.build()?;
+    graphics::set_window_title(&ctx, "Crab-A-Coconut");
+
+    Ok(())
 }
