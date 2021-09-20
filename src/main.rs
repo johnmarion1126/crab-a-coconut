d2f795440fdd54723891d812c9ba02ac1eed7052
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-09-20 00:22:24 -0700

    Create skeleton for event handler and mainstate

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,30 @@ use ggez::event;
 use ggez::graphics;
 use ggez::{Context, GameResult};
 
+struct MainState {}
+
+impl MainState {
+    pub fn new() -> Self {
+        MainState {}
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
 fn main() -> GameResult {
     let context_builder = ggez::ContextBuilder::new("Crab-A-Coconut", "John");
     let (ctx, event_loop) = context_builder.build()?;
     graphics::set_window_title(&ctx, "Crab-A-Coconut");
 
+    let state = MainState::new();
+    event::run(ctx, event_loop, state);
     Ok(())
 }
