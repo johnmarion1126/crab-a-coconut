25f53dae14c06cb007ea64f2fd0383cdf4de7dea
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-10-04 12:13:22 -0700

    Change player speed

--- src/objects/player.rs
+++ src/objects/player.rs
@@ -4,7 +4,7 @@ use ggez::input::keyboard::{self, KeyCode};
 use ggez::Context;
 
 // Constants
-const PLAYER_SPEED: f32 = 300.0;
+const PLAYER_SPEED: f32 = 400.0;
 
 pub fn move_player(pos: &mut glam::Vec2, keycode: KeyCode, x_dir: f32, ctx: &mut Context, PLAYER_WIDTH: f32, SCALE: f32) {
     let MAX_WIDTH = graphics::drawable_size(ctx).0;
